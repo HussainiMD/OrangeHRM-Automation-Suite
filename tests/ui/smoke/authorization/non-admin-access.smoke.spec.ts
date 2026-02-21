e72b037821d0b058ae037ada588d1f352bcc799c
HussainiMD <hussaini.crazystuff@gmail.com>
2026-02-21 19:14:04 +0530

    seperating test case files as per their purpose

--- tests/ui/smoke/authorization/non-admin-access.smoke.spec.ts
+++ tests/ui/smoke/authorization/non-admin-access.smoke.spec.ts
@@ -7,21 +7,21 @@ import { addNewESSUser } from "../../../../utils/users-manager.util";
 test.describe.configure({mode: "parallel", timeout: 30000});
 
 /**
- * ID from Test Cases (spreadsheet): TC_LOGIN_002
+ * ID from Test Cases (spreadsheet): TC_LOGIN_003
  */
-test('Verify Admin Users has access to Admin module', async ({page}) => {
+test('Verify ESS Users has NO access to Admin module', async ({page}, testInfo) => {
+    const user:UserType = await addNewESSUser('playwright', testInfo);    
     const navResponse:Response | null = await page.goto('/web/index.php/auth/login')
     expect(navResponse).toBeTruthy();
 
     const loginPage: LoginPage = new LoginPage(page);
-    await loginPage.signInWithCredentials({username: 'Admin', password:'admin123'});
+    await loginPage.signInWithCredentials({username: user.name, password: user.password});
     await page.waitForLoadState("networkidle");
 
     const adminLocator:Locator = page.locator('.oxd-main-menu-item--name').filter({hasText: 'Admin'});
-    expect(await adminLocator.count()).toBeGreaterThanOrEqual(1);
+    expect(await adminLocator.count()).toBe(0);//Should NOT be visible to non admin (ESS) user
 
     const userMenu: UserMenu = new UserMenu(page);
     await userMenu.logOut();
 })
-
 
