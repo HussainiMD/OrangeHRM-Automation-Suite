76ccad1dd3a7ad03003cd954ea3417a1dbb59646
HussainiMD <hussaini.crazystuff@gmail.com>
2026-02-23 22:43:53 +0530

    using pre authenticated context instead of typical login flow. This speeds up the test case execution

--- tests/ui/smoke/auth/authentication.smoke.spec.ts
+++ tests/ui/smoke/auth/authentication.smoke.spec.ts
@@ -1,4 +1,5 @@
-import { test, expect, Locator, Response } from "@playwright/test";
+import { expect, Locator, Response } from "@playwright/test";
+import {test} from "../../../../fixtures/admin-auth.fixtures";
 import LoginPage from "../../../../pages/LoginPage";
 import UserMenu from "../../../../pages/components/UserMenu";
 import credentials from "../../../types/credentials";
@@ -6,23 +7,14 @@ import credentials from "../../../types/credentials";
 /**
  * ID from Test Cases (spreadsheet): TC_LOGIN_001
  */
-test('user can login, see profile button, and logout', async ({page}) => {    
-    
-    const loginNavResponse: Response | null = await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login', {waitUntil: "networkidle"});
-    if(!loginNavResponse) {
-       console.log('unable to navigate to login page');
-       return;
-    } 
+test('user can login, see profile button, and logout', async ({adminUserAuthPage}) => {        
+    test.slow(); //app is taking longer to load the landing page, hence marking it as slow
+    const navResponse: Response |null = await adminUserAuthPage.goto('https://opensource-demo.orangehrmlive.com/web/index.php', {waitUntil: "networkidle"});
+    expect(navResponse).toBeTruthy();
 
-    const loginPage:LoginPage = new LoginPage(page);
-    const loginCredentials:credentials = {username: 'Admin', password: 'admin123'};
-    await loginPage.signInWithCredentials(loginCredentials);
-
-    const dropDown:Locator = page.locator("span[class $= 'userdropdown-tab']");
+    const dropDown:Locator = adminUserAuthPage.locator("span[class $= 'userdropdown-tab']");
     expect(await dropDown.count()).toBe(1);    
 
-    const userMenu:UserMenu = new UserMenu(page);
-    await userMenu.logOut();
-    
-    await page.waitForTimeout(3000);
+    const userMenu:UserMenu = new UserMenu(adminUserAuthPage);
+    await userMenu.logOut();    
 })
