b047abe7fb3bbafe90511461191ff13614c5b49c
HussainiMD <hussaini.crazystuff@gmail.com>
2026-03-08 22:29:52 +0530

    test case to verify user activity after session is expired

---
+++ tests/ui/smoke/auth/userActivityInExpiredSession.spec.ts
@@ -0,0 +1,24 @@
+import {test, expect, Locator} from "../../../../fixtures/essUser-auth.fixture";
+
+/**
+ * ID from Test Cases (spreadsheet): TC_LOGIN_011
+ */
+test('User Session Expired - Re-login enforced on User Action', async ({essUserAuthPage, essUserAuthContext, logger}) => {
+   
+   //extracting domain name from page URL
+   const domainMatchArray: RegExpMatchArray | [] = essUserAuthPage.url().match(/[://]\/\/(.+[.com])\/.*/i) ?? [];
+   expect(domainMatchArray.length).toBeGreaterThan(0);
+
+   const domain: string = domainMatchArray[1] ?? '';   
+   
+   /**Similating session expiry by clearing cookies */ 
+   await essUserAuthContext.clearCookies({domain});
+   
+   /*Clicking on UI element after session is expired, it automatically redirects user to login page*/
+   const myInfoBtnLocator: Locator = essUserAuthPage.locator('.oxd-sidepanel a.oxd-main-menu-item').filter({hasText: 'My Info'});
+   await myInfoBtnLocator.click();
+   await essUserAuthPage.waitForLoadState('networkidle');
+   
+   expect(essUserAuthPage.url()).toContain('/auth/login');   
+})
+
