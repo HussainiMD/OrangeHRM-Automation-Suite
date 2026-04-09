612f8c9378634d8c855c035c8289210052034ee1
HussainiMD <hussaini.crazystuff@gmail.com>
2026-04-09 15:52:15 +0530

    Mark back button test as failed due to known bug in the app

--- tests/ui/regression/auth/sessionManagement.spec.ts
+++ tests/ui/regression/auth/sessionManagement.spec.ts
@@ -7,6 +7,8 @@ const dashboardURLRegEx: RegExp = /dashboard/i;
  * Verifies the scenario where user clicks on browser back button after successful login to the AUT
 */
 test('Browser Back Button After Login', async ({essUserAuthPage}) => {    
+    /*BUG: up on back button, app is going to login page despite active session. Ideally it should go to dashboard page*/
+    test.fail(true, 'Known bug in the app. Developers are to be notified'); //marking it as failure as this test case will fail all the time till fixed
     await essUserAuthPage.goBack();   
     await expect(essUserAuthPage).toHaveURL(dashboardURLRegEx);
 });
